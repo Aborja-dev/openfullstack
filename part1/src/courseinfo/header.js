a7647cdf7e24e44f602537ef8ba5e99ae52cc072
Aborja-dev <75704198+Aborja-dev@users.noreply.github.com>
2021-10-24 18:55:59 -0500

    Ejercicio 1

---
+++ part1/src/courseinfo/header.js
@@ -0,0 +1,10 @@
+const Header = ()=>{
+   const course = 'Half Stack application development'
+   return(
+      <>
+         <h2>{course}</h2>
+      </>
+   )
+}
+export default Header
+
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+//Components
+import Header from './courseinfo/header'
+import Content from './courseinfo/content'
+import Total from './courseinfo/total'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div>
+      <h1>Informacion del curso</h1>
+      <Header/>
+      <Content/>
+      <Total/>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
